3c7b0ffa6671ce1c56687a16a915672465bbdea6
micaelaasteE <micaelaaste1@gmail.com>
2025-09-24 12:45:31 -0300

    contacts details page

--- src/app/pages/contact-details-page/contact-details-page.ts
+++ src/app/pages/contact-details-page/contact-details-page.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-
+import { Component, inject, input } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { Contact } from '../../interfaces/contact';
+import { ContactService } from '../../services/contact-services';
 @Component({
   selector: 'app-contact-details-page',
   imports: [],
@@ -7,5 +9,13 @@ import { Component } from '@angular/core';
   styleUrl: './contact-details-page.scss'
 })
 export class ContactDetailsPage {
+  contactsService = inject (ContactService)
 
+  idContacto = input<number>(); 
+
+  contact: Contact | undefined = undefined;
+
+  async ngOnInit(){
+    this.contact = await this.contactsService.getContactById(this.id()!)
+  }
 }

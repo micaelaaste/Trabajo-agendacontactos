f2292df30b4c9e8864254918ba5dba2417128ae2
micaelaasteE <micaelaaste1@gmail.com>
2025-10-08 09:29:07 -0300

    spinner 2

--- src/app/pages/add-contact/add-contact.ts
+++ src/app/pages/add-contact/add-contact.ts
@@ -4,10 +4,11 @@ import { Contact, NewContact } from '../../interfaces/contact';
 import { AuthService } from '../../services/auth-service';
 import { ContactService } from '../../services/contact-services';
 import { Router } from '@angular/router';
+import { Spinner } from '../../components/spinner/spinner';
 
 @Component({
   selector: 'app-add-contact',
-  imports: [FormsModule],
+  imports: [FormsModule, Spinner],
   templateUrl: './add-contact.html',
   styleUrl: './add-contact.scss'
 })
@@ -17,7 +18,8 @@ export class AddContact implements OnInit {
   router = inject(Router);
   idContacto = input<number>();
   contactoOriginal: Contact | undefined = undefined;
-  form = viewChild<NgForm>('newContactForm')
+  form = viewChild<NgForm>('newContactForm');
+  isLoading = false; 
 
   async ngOnInit() {
     console.log(this.idContacto());
@@ -38,12 +40,14 @@ export class AddContact implements OnInit {
 
   async handleFormSubmission(newContact: NewContact) {
     let res;
+    this.isLoading= true
     if (this.idContacto()) {
       res = await this.contactService.editContact({ ...newContact, id: this.idContacto()!.toString() })
     } else {
       res = await this.contactService.createContact(newContact);
     }
-  }
+    this.isLoading = false 
+    }
 
   async createContact(form: any) {
     const nuevoContacto: NewContact = {
@@ -63,3 +67,7 @@ export class AddContact implements OnInit {
   }
 
 }
+  function createContact(form: any, any: any) {
+  throw new Error('Function not implemented.');
+}
+
